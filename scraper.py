eef7d6e71f06b27f69b2b28b72974adf69fb26f9
dtadmor <bucsno1@gmail.com>
2018-10-04 17:47:50 -0400

    Added bball reference scraper

--- scraper.py
+++ scraper.py
@@ -2,13 +2,12 @@ import pandas as pd
 import requests
 import regex as re
 import time
-
+from bs4 import BeautifulSoup
 
 # Creating a function to create a list of seasons to scrape
 def past_seasons(n_years, end_year=18):
 
     # Create a list of years
-    end_year = end_year
     seasons = []
     for i in range(1, n_years + 1):
         start_year = 18 - i
@@ -17,10 +16,6 @@ def past_seasons(n_years, end_year=18):
         end_year = start_year
 
     return seasons
-
-
-
-
 
 # Creating a function to scrape the NBA wesbite
 def nba_scraper(url, headers, n_years, verbose):
@@ -59,3 +54,38 @@ def nba_multi_scraper(url_dict, headers, n_years, verbose=0):
             print('Completed scrape of {}'.format(key))
 
     return data_dict
+
+# Creating a function to scrape bball reference for total stats
+def bball_reference_scraper(url, n_years, verbose):
+
+    # Initiate variables for the for loop
+    season_list = [str(year) for year in range(2018, 2018 - n_years, -1)]
+    i = 1
+    have_df = False
+
+    for season in season_list:
+
+        # Creating the new url for the this season and scraping data
+        url = re.subf(r'NBA_[\d]+', 'NBA_{}'.format(season), url)
+        result = requests.get(url)
+        content = result.content.decode('utf-8')
+        soup = BeautifulSoup(re.sub('<!--|-->', '', content), 'lxml')
+
+        # Inputting the column names if they aren't there yet
+        if have_df == False:
+            columns = [item.text for item in soup.find('table', {'id': 'team_shooting'}).find('thead').find_all('tr')[2].find_all('th')]
+            df = pd.DataFrame(columns=columns)
+            have_df = True
+
+        # Adding the shooting data to the dataframe
+        shooting = ['']
+        shooting.extend([item.text for item in soup.find('table', {'id': 'team_shooting'}).find('tfoot').find_all('td')])
+        df.loc[season] = shooting
+
+        # Updating the user and getting ready to scrape again
+        if verbose > 0:
+            print('Completed Scrape {} of {}'.format(i, len(season_list)))
+        i += 1
+        time.sleep(5)
+
+    return df
